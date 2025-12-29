822a66e25c9fbe0f0470640e423f8719dd840216
zz <869969681@qq.com>
2025-12-29 20:22:47 +0800

    del

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ from bot.tgb import router as bot_router
 from databases.kv import router as kv_router
 from api.mtproto import router as mtproto_router
 from api.ffmpeg import router as ffmpeg_router
-from api.xuexi import router as tv_router
 
 
 def create_app() -> FastAPI:
@@ -16,7 +15,6 @@ def create_app() -> FastAPI:
     app.include_router(kv_router, tags=['redis'])
     app.include_router(mtproto_router, prefix='/svc', tags=['tgapi'])
     app.include_router(ffmpeg_router, prefix='/svc', tags=['service'])
-    app.include_router(tv_router, prefix='/live', tags=['tv'])
     app.middleware('http')(middleware)
 
     @app.get("/", include_in_schema=False)
